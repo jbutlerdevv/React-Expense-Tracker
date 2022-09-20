dcb40bdc92cddfe6af95d1b2b957b6faa6ac25e5
Josh Butler <joshorion7@gmail.com>
2022-09-20 17:41:54 -0400

    props and component inception, LOL

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
-import React from "react";
-import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 8, 17);
-    const expenseTitle = "Car Payment";
-    const expenseAmount = 450;
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
