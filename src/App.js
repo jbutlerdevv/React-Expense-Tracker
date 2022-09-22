86a146d681b83d44f9d8d585431ab2c2909a173a
joshorion7 <joshorion7@gmail.com>
2022-09-21 22:39:18 -0400

    re-implemented ExpenseDetail using map

--- src/App.js
+++ src/App.js
@@ -1,38 +1,67 @@
 // import ExpenseItem from "./components/ExpenseItem";
 import ExpenseDetails from "./components/ExpenseDetails";
 
+export const expensesArr = [
+    {
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: "e2",
+        title: "New TV",
+        amount: 799.49,
+        date: new Date(2021, 2, 12),
+    },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
+
 function App(props) {
-    const expenses = [
-        {
-            id: "e1",
-            title: "Toilet Paper",
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: "e2",
-            title: "New TV",
-            amount: 799.49,
-            date: new Date(2021, 2, 12),
-        },
-        {
-            id: "e3",
-            title: "Car Insurance",
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: "e4",
-            title: "New Desk (Wooden)",
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    // COURSE SOLUTION
+    // const expenses = [
+    //     {
+    //         id: "e1",
+    //         title: "Toilet Paper",
+    //         amount: 94.12,
+    //         date: new Date(2020, 7, 14),
+    //     },
+    //     {
+    //         id: "e2",
+    //         title: "New TV",
+    //         amount: 799.49,
+    //         date: new Date(2021, 2, 12),
+    //     },
+    //     {
+    //         id: "e3",
+    //         title: "Car Insurance",
+    //         amount: 294.67,
+    //         date: new Date(2021, 2, 28),
+    //     },
+    //     {
+    //         id: "e4",
+    //         title: "New Desk (Wooden)",
+    //         amount: 450,
+    //         date: new Date(2021, 5, 12),
+    //     },
+    // ];
+    // END COURSE SOLUTION
 
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseDetails items={expenses} />
+            <ExpenseDetails items={expensesArr} />
         </div>
     );
 }
--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,32 +1,44 @@
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseDetails.css";
+import { expensesArr } from "../App";
 import Card from "./Card";
 
 function ExpenseDetails(props) {
-    return (
-        <Card className='expenses'>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
-        </Card>
-    );
+    const expensesData = expensesArr.map((expense, i) => (
+        <ExpenseItem
+            key={i}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+    ));
+
+    return <Card className='expenses'>{expensesData}</Card>;
+
+    // return (
+    //     <Card className='expenses'>
+    //         <ExpenseItem
+    //             title={props.items[0].title}
+    //             amount={props.items[0].amount}
+    //             date={props.items[0].date}
+    //         />
+    //         <ExpenseItem
+    //             title={props.items[1].title}
+    //             amount={props.items[1].amount}
+    //             date={props.items[1].date}
+    //         />
+    //         <ExpenseItem
+    //             title={props.items[2].title}
+    //             amount={props.items[2].amount}
+    //             date={props.items[2].date}
+    //         />
+    //         <ExpenseItem
+    //             title={props.items[3].title}
+    //             amount={props.items[3].amount}
+    //             date={props.items[3].date}
+    //         />
+    //     </Card>
+    // );
 }
 
 export default ExpenseDetails;
